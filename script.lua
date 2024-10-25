a79684a60dcd78d4c9509f899b0ece91b8ffe4ff
Skylar <commit@nobleskye.dev>
2024-10-24 21:58:55 -0700

    finally at last it works!!

--- script.lua
+++ script.lua
@@ -4,8 +4,6 @@
 function events.item_render(item)
     local itemName = item:getName():lower() 
     local tex = textures[itemName]
-    -- log(itemName)
-    log(tex)
   if tex then
     return models.model.Item:setPrimaryTexture("CUSTOM",tex)
   end
