30fb287e5a2228455a7180bbb6462cfc98ded8fc
Skylar <commit@nobleskye.dev>
2024-10-24 22:22:39 -0700

    sciprt

--- script.lua
+++ script.lua
@@ -3,7 +3,7 @@ function events.item_render(item)
     local itemName = item:getName():lower() 
     local tex = textures[itemName]
   if tex then
-    return models.model.Item:setPrimaryTexture("CUSTOM",tex)
+    return models.model.Item:setPrimaryTexture("CUSTOM",'undertale'..tex)
   end
 end
 
